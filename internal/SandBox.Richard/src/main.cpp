dc3129da2afdc48f89fde3e882f7a54b7893ce98
IvanPostu <ipostu20000127@gmail.ru>
2022-08-06 17:23:44 +0300

    Change project structure

---
+++ internal/SandBox.Richard/src/main.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <cstdio>
+
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+  printf("1234\n");
+
+  return EXIT_SUCCESS;
+}
--- scripts/configure.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cmake -S ./ -B ./build/Debug -Dtest=ON -DCMAKE_BUILD_TYPE=Debug
-
-
