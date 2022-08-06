84179785b349b88ba346ea8f19798c3fb2cba339
IvanPostu <ipostu20000127@gmail.ru>
2022-08-06 18:24:59 +0300

    Change project

--- internal/SandBox.Richard/src/main.cpp
+++ internal/SandBox.Richard/src/main.cpp
@@ -1,13 +1,50 @@
 #include <cstdlib>
 #include <cstdio>
 
+#include "Adder/adder.hpp"
+#include <GLFW/glfw3.h>
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
 
-  printf("1234\n");
+  float f = add(2.2f, 3.3f);
+
+  printf("%s - %f\n", "S1impleProject", f);
+
+  GLFWwindow *window;
+  int width, height;
+
+  if (!glfwInit())
+  {
+    fprintf(stderr, "Failed to initialize GLFW\n");
+    exit(EXIT_FAILURE);
+  }
+
+  
+  glfwWindowHint(GLFW_DEPTH_BITS, 16);
+  glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+
+  window = glfwCreateWindow(300, 300, "Gears", NULL, NULL);
+  if (!window)
+  {
+    fprintf(stderr, "Failed to open GLFW window\n");
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  while (!glfwWindowShouldClose(window))
+  {
+
+
+    // Swap buffers
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  // Terminate GLFW
+  glfwTerminate();
 
   return EXIT_SUCCESS;
 }
